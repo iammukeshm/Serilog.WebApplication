5c9455eb88ac24c247288abdcaa6096e646ee850
Mukesh Murugan <iammukeshm@gmail.com>
2020-05-23 20:52:34 +0530

    Add project files.

---
+++ Serilog.WebApplication/Pages/Index.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace Serilog.WebApplication.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            _logger.LogInformation("Requested the Index Page");
+            int count;
+            try
+            {
+                for(count = 0;count<=5;count++)
+                {
+                    if(count==3)
+                    {
+                        throw new Exception("Random Exception Occured");
+                    }
+                    else
+                    {
+                        _logger.LogInformation("Iteration Count is {iteration}", count);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                _logger.LogError(ex,"Exception Caught");
+            }
+        }
+    }
+}
---
+++ Serilog.WebApplication/Program.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Serilog.WebApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Read Configuration from appSettings
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            //Initialize Logger
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(config)
+                .CreateLogger();
+            try
+            {
+
+                Log.Information("Application Starting.");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "The Application failed to start.");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+            
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseSerilog() //Uses Serilog instead of default .NET Logger
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
